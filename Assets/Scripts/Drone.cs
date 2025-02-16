09777fa65ed29f3f5741082d95cdee62b9431d38
multilococ <multilococ@yandex.ru>
2025-02-16 15:57:24 +0300

    Add Vector3Exstensions

--- Assets/Scripts/Drone.cs
+++ Assets/Scripts/Drone.cs
@@ -6,6 +6,8 @@ public class Drone : MonoBehaviour
 {
     [SerializeField] private Transform[] _wayPoints;
 
+    private float _minDistanceToTarget = 0.1f;
+
     private Mover _mover;
 
     private int _currentWayPointIndex = 0;
@@ -20,21 +22,21 @@ public class Drone : MonoBehaviour
     {
         ChooseWayPoint();
     }
-
+    
     private void ChooseWayPoint()
     {
-        if (_wayPoints.Length < 1)
+        if (_wayPoints.Length  < 1)
         {
             throw new InvalidOperationException();
         }
         else
         {
-            if ((transform.position - _wayPoints[_currentWayPointIndex].position).sqrMagnitude == 0)
-            {
+            if(transform.position.IsEnoughClose(_wayPoints[_currentWayPointIndex].position, _minDistanceToTarget))
+            { 
                 _currentWayPointIndex = (_currentWayPointIndex + 1) % _wayPoints.Length;
             }
 
-            _mover.SetTarget(_wayPoints[_currentWayPointIndex]);
+                _mover.SetTarget(_wayPoints[_currentWayPointIndex]);
         }
     }
 }
